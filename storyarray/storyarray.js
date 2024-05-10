8238978fdc5626f1c0ca7cfe4337632847483a7c
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-05-10 17:48:08 -0500

    added a few object to store our story text into

---
+++ storyarray/storyarray.js
@@ -0,0 +1,19 @@
+const level_1_Story ={
+    storyStart: "story start text",
+    questions1: ["you win text", "you die"],
+    questions2: ["you win text", "you die"],
+    questions3: ["you win text", "you die"]
+}
+
+const level_2_Story = {
+    questions1: ["you win text", "you die"],
+    questions2: ["you win text", "you die"],
+    questions3: ["you win text", "you die"]
+}
+
+const level_3_Story = {
+    questions2: ["you win text", "you die"],
+    questions2: ["you win text", "you die"],
+    questions3: ["you win text", "you die"],
+
+}
