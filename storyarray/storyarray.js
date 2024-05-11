f25fb0a22a69448a73e7a00c1c5d2a32817488af
Anna Manrodt <157744313+AnnaManrodt@users.noreply.github.com>
2024-05-11 14:02:53 -0500

    added to the story array

--- storyarray/storyarray.js
+++ storyarray/storyarray.js
@@ -1,8 +1,8 @@
 const level_1_Story ={
-    storyStart: "story start text",
-    questions1: ["you win text", "you die"],
-    questions2: ["you win text", "you die"],
-    questions3: ["you win text", "you die"]
+    storyStart: [`You are a well-experienced adventurer traveling to the next kingdom.  You awaken in your camp under a small tree feeling well rested for the journey ahead.  You pull out your map and examine it for the next kingdom.  You see a shortcut a villager marked on your map the day before.` ,`Do you take the villager’s shortcut or proceed to the well-worn path?`, `You head down the main path.`, `You head to the short cut`],
+    questions1: [`Clearly, you are one for reliability and stability. Unfortunately, reliability and stability do not make for a good adventurer. To be a good adventurer one must take the path less traveled. The main path leads you to a nice town you settle down in and never go on another adventure ever again!`, `You head into the forest and take the shortcut.  Not long into the day, you hear the frightened cries of a young woman nearby. As you approach, you see the young woman is Princess Katy from Dizzylot, the kingdom you are traveling to. She is tied to a tree. `],
+    questions2: [`Do you cut the rope and free the princess or do you leave her there?`, `Cut the rope.`, `You go up to the princess as she struggles and say “You’re Princess Katy from the kingdom of Dizzylot, right? Are you okay? Do you need help? ”`, `She replies “Of course I am, What do you think? I'm tied to a tree!”`, `You lean down to untie her and feel a sharp pain in the middle of your back. Blood sprays onto the princess' face as she looks up at you with an evil grin.`, `The princess says “Nice shot boys! They are sure to have plenty of a loot on them”`, `You died`],
+    questions3: [`You start to walk up to the princess but hear rustling in the leaves nearby and notice that the ropes tying her to the tree look strangely loose. As you start to draw your weapon and prepare for the attack you are sure is coming, you remember the words of a wise professor… `, `You get lost in thought tearing up at the thought of your old classmates and teacher. A Bandit comes up and kills you, you should have indented. `, `Remembering your professor's wise words has focused your senses, you spin around your sword cutting down the first bandit that dared to make a move. You make quick work of the other three but when you turn to look for the princess she is gone. `]
 }
 
 const level_2_Story = {
@@ -15,5 +15,5 @@ const level_3_Story = {
     questions2: ["you win text", "you die"],
     questions2: ["you win text", "you die"],
     questions3: ["you win text", "you die"],
-
+    storyEnd:""
 }
