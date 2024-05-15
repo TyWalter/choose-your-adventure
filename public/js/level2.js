9b882eb5224019904a19fc01c485c7aee42ee368
JKrech01 <funkjackie@gmail.com>
2024-05-14 22:05:55 -0500

    modified js for  level 2 and 3

--- public/js/level2.js
+++ public/js/level2.js
@@ -8,9 +8,6 @@
 
 // const level_2_Story = {
 //     // storyStart: 
-//     questions2: [`“Pffs why should I care about some old half-taxidermied dog, he probably was ready to die!!” `, `The Princess replies “HOW DARE YOU!!!” She scratches and lunges towards you with a dagger she had hidden behind her. “How dare you not grieve for Dizzy the all creator?”`, `The blade hits you first on the left side of your stomach, then, right then, in the middle… she stabs you over and over until everything goes black.`, `You die `, `Dizzy the creator was murdered! Of course, I’ll avenge Dizzy, that poor old boy, where is his murderer?” `, `“It's an assassin who goes by the name of CSS.  Many have fallen to her stylish and well-organized blade. We will have to be careful of her followers’ bootstrap and flex-box as well.” `, `You follow the Princess to the swamp where she lives and prepare to do battle again. In the swamp, there is a small shack of a house. “This is it,” the Princess says. `, `You go into the house sword at the ready for whatever comes your way. Just as you enter the door you think about a great thing Dizzy always said “Go to where he lives in a swamp and engage in battle.” `,],
-//     questions3: [`As the door opens you see three figures standing by the fireplace of the small house ready to attack. ‘Damn!” you think, “we were too loud, we are going to die”. As soon as the door opens there is a dagger square in your chest, but you can't stop now. You fight through the pain and move forward sloppily.`, `You hear a cry behind you. The Princess has been slain. You swing at CSS but miss. She laughs in your face, “Trying to assassinate the assassin? How cute.” She kicks you down and you fall with a grunt. “What do you think boy, should we have some fun?” They laugh cruelly at your pain. You die slowly over the next two hours as the assassins torture you for being so arrogant as to think you could defeat CSS and her frameworks. `, `The noise of your entrance startles the sleeping assassins. You see the one they call Bootstrap rises from his cot on the ground with a look of surprise. You go forward slicing him in half before he has time to draw his blade. `, `You hear a noise behind you, it's Princess Katy slaying Flex-box where he stands. You feel a blade fly by your right ear barely missing you. You swing around and engage the infamous and very beautiful killer known only as CSS. She lunges towards you, teeth bared and eyes bright. You dodge, but a split second too late and the blade grazes your arm. It's your turn as you step forward and thrust your blade into her stomach. Her face displays a look of surprise and pain. “Why?’ she asks as she falls to the floor and dies. `]
-// }
 
 const story = {
     part1: {
@@ -22,26 +19,31 @@ const story = {
         //img: $('body').css('background-image', 'url("../../public/imgs/deathscreen.PNG")'),
     },
     part2: {
-        description: `You follow the footprints into the woods, the forest grows thick as you run deeper and deeper through it. 
-            At this point, almost no sunlight is breaking through the thick dense forest treetop. You see a figure just barely in front of you. 
-            Wait, let me explain,” the Princess says. 
-            “You have one minute.” You reply. I left the royal life after the death of my dog Dizzy. He actually was the creator of my kingdom and closer to a god than a real dog, but he was old and weak and someone murdered him., He was my best friend. I left to find his murderer, joined this group of bandits and became their leader. Through their connections, I learned the name of Dizzy’s killer, who happens to have an assassin in the marshes.`,
+        description: `You follow the footprints into the woods, the forest grows thick as you run deeper and deeper through it. At this point, almost no sunlight is breaking through the thick dense forest treetop. You see a figure just barely in front of you.`,
+        description: `Wait, let me explain,” the Princess says. “You have one minute.” You reply.`,
+        description: `I left the royal life after the death of my dog Dizzy. He actually was the creator of my kingdom and closer to a god than a real dog, but he was old and weak and someone murdered him.`, 
+        description: `He was my best friend. I left to find his murderer, joined this group of bandits and became their leader. Through their connections, I learned the name of Dizzy’s killer, who happens to have an assassin in the marshes.`,
         question: `If you help me, I'll make sure you're knighted and that you are declared a hero of the realm. What do you say?”`,
         wrongChoice: `No`,
         rightChoice: `Yes`,
-        wrongResult: `“Pffs why should I care about some old half-taxidermied dog, he probably was ready to die!!”, The Princess replies “HOW DARE YOU!!!” She scratches and lunges towards you with a dagger she had hidden behind her. “How dare you not grieve for Dizzy the all creator?”, The blade hits you first on the left side of your stomach, then, right then, in the middle… she stabs you over and over until everything goes black., You die `,
+        wrongResult: `“Pffs why should I care about some old half-taxidermied dog, he probably was ready to die!!”`,
+        description: `The Princess replies “HOW DARE YOU!!!” She scratches and lunges towards you with a dagger she had hidden behind her. “How dare you not grieve for Dizzy the all creator?”`,
+        description: `The blade hits you first on the left side of your stomach, then, right then, in the middle… she stabs you over and over until everything goes black.`,
         //img: $('body').css('background-image', 'url("../../public/imgs/blood.PNG")'),
+        description: `You die`,
         // img: $('body').css('background-image', 'url("../../public/imgs/deathscreen.PNG")'),
     },
     part3: {
-        description: `Dizzy the creator was murdered! Of course, I’ll avenge Dizzy, that poor old boy, where is his murderer?”, “It's an assassin who goes by the name of CSS.  Many have fallen to her stylish and well-organized blade. We will have to be careful of her followers’ bootstrap and flex-box as well.” `,
+        description: `Dizzy the creator was murdered! Of course, I’ll avenge Dizzy, that poor old boy, where is his murderer?”,`
+        description: `“It's an assassin who goes by the name of CSS.  Many have fallen to her stylish and well-organized blade. We will have to be careful of her followers’ bootstrap and flex-box as well.” `,
         //img: $('body').css('background-image', 'url("../../public/imgs/swamp.PNG")'),
-        description: `You follow the Princess to the swamp where she lives and prepare to do battle again. In the swamp, there is a small shack of a house. “This is it,” the Princess says. You go into the house sword at the ready for whatever comes your way. Just as you enter the door you think about a great thing Dizzy always said “Go to where he lives in a swamp and engage in battle.” `,
+        description: `You follow the Princess to the swamp where she lives and prepare to do battle again. In the swamp, there is a small shack of a house. “This is it,” the Princess says.`,
+        description: `You go into the house sword at the ready for whatever comes your way. Just as you enter the door you think about a great thing Dizzy always said “Go to where he lives in a swamp and engage in battle.” `,
         question: `Do you enter the house?`,
         wrongChoice: `No`,
         rightChoice: `Yes`,
-        wrongResult: `As the door opens you see three figures standing by the fireplace of the small house ready to attack. ‘Damn!” you think, “we were too loud, we are going to die”. As soon as the door opens there is a dagger square in your chest, but you can't stop now. You fight through the pain and move forward sloppily.`, 
-        description:`You hear a cry behind you. The Princess has been slain. You swing at CSS but miss. She laughs in your face, “Trying to assassinate the assassin? How cute.” She kicks you down and you fall with a grunt. “What do you think boy, should we have some fun?” They laugh cruelly at your pain. You die slowly over the next two hours as the assassins torture you for being so arrogant as to think you could defeat CSS and her frameworks.`,
+        wrongResult: `As the door opens you see three figures standing by the fireplace of the small house ready to attack. ‘Damn!” you think, “we were too loud, we are going to die”. As soon as the door opens there is a dagger square in your chest, but you can't stop now. You fight through the pain and move forward sloppily.`,
+        description: `You hear a cry behind you. The Princess has been slain. You swing at CSS but miss. She laughs in your face, “Trying to assassinate the assassin? How cute.” She kicks you down and you fall with a grunt. “What do you think boy, should we have some fun?” They laugh cruelly at your pain. You die slowly over the next two hours as the assassins torture you for being so arrogant as to think you could defeat CSS and her frameworks.`,
         //img: $('body').css('background-image', 'url("../../public/imgs/deathscreen.PNG")'),
         rightResult: `The noise of your entrance startles the sleeping assassins. You see the one they call Bootstrap rises from his cot on the ground with a look of surprise. You go forward slicing him in half before he has time to draw his blade.`,
         description: `You hear a noise behind you, it's Princess Katy slaying Flex-box where he stands. You feel a blade fly by your right ear barely missing you. You swing around and engage the infamous and very beautiful killer known only as CSS. She lunges towards you, teeth bared and eyes bright. You dodge, but a split second too late and the blade grazes your arm. It's your turn as you step forward and thrust your blade into her stomach. Her face displays a look of surprise and pain. “Why?’ she asks as she falls to the floor and dies.`,
