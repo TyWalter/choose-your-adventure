38fe66eb50ebc6d1623d618d6427c798431609fb
TyWalter <walterty89@gmail.com>
2024-05-17 00:03:41 -0500

    Added hbs helpers and a few css changes

--- controllers/routes/game.js
+++ controllers/routes/game.js
@@ -4,12 +4,13 @@ const {Login} = require("../../models");
 // Get all game info
 router.get("/", async (req, res) => {
   try {
+    const url = req._parsedOriginalUrl.pathname
     // const result = await Login.findByPk(req.session.login_id, {
     //   attributes: {exclude: ['password']}
     // });
     // const user = result.get({plain:true});
     // res.json({status: "success", payload: result});
-    res.render('level1', {layout: 'game'})
+    res.render('level1', {layout: 'game', url})
   }catch(err){
     res.status(400).json({ status:"error" });
   };
@@ -18,9 +19,10 @@ router.get("/", async (req, res) => {
 //get game info by ID
 router.get("/:id", async (req, res) => {
   try {
+    const url = req._parsedOriginalUrl.pathname
     // const result = await Login.findByPk(req.params.id)
     // res.json({status: "success", payload: result});
-    res.render(`level${req.params.id}`, {layout: 'game'})
+    res.render(`level${req.params.id}`, {layout: 'game', url})
   }catch(err){
     res.status(400).json({ status:"error" });
   };
