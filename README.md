d62fcb8565ef0590adc435e674111098afd1d834
TyWalter <walterty89@gmail.com>
2024-05-19 20:52:33 -0500

    good

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 # Group Project 2:  
 The Queen and Her Guard
 
+
 ## Description
 The Queen and Her Guard is a narrative game where the player takes on the role of a new guard joining the Queen's royal forces. The player will make choices that determine their progression through the story and relationship with other characters.
 
-## Group Collaboration: (Keep it to FIVE?)
+## Group Collaboration:
 Challenges:
 - Merging storylines with different branches
 - Creating character profiles using Handlebars templates with data from a Sequelize Character model
@@ -12,7 +13,7 @@ Challenges:
 - 
 - 
 
-## Team Wins: (keept it to FIVE?)
+## Team Wins:
 - Successfully implemented Handlebars templates to dynamically render character profiles
 - Built out branching storylines using Handlebars conditionals and partials
 - Rendered character profiles and story pages dynamically using Handlebars templates with data from Sequelize Character and Story models
@@ -28,11 +29,14 @@ Challenges:
 - 
 
 ## Visuals
-![Screenshot of Homepage](imgs\readmeimgs\homepage.png)
-![Screenshot of Profile Page](imgs\readmeimgs\homepage.png)
-![Screenshot of a Story Page](imgs\readmeimgs\homepage.png)
+![Screenshot of Homepage](xxxxxxxxxxx)
+![Screenshot of Profile Page](xxxxxxxxxxx)
+![Screenshot of a Story Page](xxxxxxxxxxx)
 
-## Tech Stack: (indicate NEW technology)
+
+## Installation
+
+## Tech Stack:
 - Node.js
 - Express.js
 - Sequelize ORM
@@ -46,10 +50,15 @@ Challenges:
 - 
 - 
 ## Links:
-- [Link to deployed version on Render:](https://choose-your-adventure-qxmy.onrender.com/)
-- [Repo:](https://github.com/TyWalter/choose-your-adventure)
-- Presentation Slides: [https://docs.google.com/presentation/d/1q8bhCSvRCu8qISwdX4nvC8TBr6nGyE28BZgYH8RfEvA/edit#slide=id.g271b3686168_0_70 ]
-- [Wireframe:] imgs\wireframe.png
+- Link to deployed version on Render:
+- Repo:
+- Presentation Slides:
+- Google Docs Working Dcoument:
+- Wireframe:
+
+
+## Usage
+
 
 ## Acknowledgments
 * Thank you to [Dotenv](https://www.npmjs.com/package/dotenv)
@@ -57,19 +66,15 @@ Challenges:
 * Thank you to [Sequelize](https://www.npmjs.com/package/sequelize)
 * Thank you to [Bcrypt](https://www.npmjs.com/package/bcrypt)
 * Thank you to [FlatIcon](https://www.flaticon.com/)
-* Thank you to [Font Awesome](https://fontawesome.com/)
-* Thank you to [Google Fonts](https://fonts.google.com/)
-*  Thank you to [typeit.js for the typing animation] (https://www.typeitjs.com/)
 
 ## Authors / Collaborators
+* [Jackie Funk]()
+* [Anna Manrodt]()
+* [Adam Shew]()
 * [Tyler Walter](https://github.com/TyWalter)
-* [Anna Manrodt](https://github.com/AnnaManrodt)
-* [Adam Shew](https://github.com/Adam-Shew)
-* [Jackie Funk](https://github.com/JKrech01)
+
+## Support
 
 ## Project Requirements:
 - [Project 2 Requirements and Notes](https://docs.google.com/document/d/1fJ2dYtbmMBDxmZlDZ3rP0PYQKk4UAzI7jzBZD0HVC4w/edit)
 
-## Usage
-## Installation
-## Support
--- public/js/level3.js
+++ public/js/level3.js
@@ -4,35 +4,41 @@ const story = {
         description: `“You're not a half-bad fighter, you'll make a great knight.” Princess Katy says`,
         // img: $('body').css('background-image', 'url("imgs/alterending.PNG")')
         question: `“well, I suppose it's time to head back to Dizzylot, should we go?” `,
-        wrongChoice: `I helped you avenge Dizzy, I didnt agree to do anything else` ,
+        wrongChoice: `I helped you average dizzy I didnt agree to do anything` ,
         rightChoice: `Sure! Let's go`,
-        wrongResult:  `“Fine,” she says and you go your separate ways. You continue to perform good deeds, but songs are never written about you. You kill bad guys and help good people, but no one knows your name.  You lead a life of purpose and anonymity. Due to this, and your humble nature, you die alone and penniless at an old age. The world will never know the deeds you did and no one remembers you.`,
+        wrongResult:  `“Fine,” she says.  You go your separate ways.You continue to do good deeds but songs are never written about you.  You kill bad guys and help good people, but no one knows your name.  You lead a life of purpose and anonymity. Due to this, and your humble nature, you die alone and penniless in old age. The world will never know the deeds you did and no one remembers you.`,
          // img: $('body').css('background-image', 'url("imgs/castleimg.PNG")')
     },
     part2: {
-        description: `You arrive at the gates of the castle, but the guards stop you and ask “Who are you two? The King is not accepting visitors.”`,
+        description: `You arrive at the gates of the castle, but the guards stop you and ask “Who are you two? The king is not accepting visitors.”`,
         // img: $('body').css('background-image', 'url("imgs/JAIL CELL.PNG")')
         question: `What do you do? `,
         wrongChoice: `Kill the guards`,
         rightChoice: `Convince them to let you in`,
-        wrongResult: `You step up to the guards as if you're going to chat with them and quickly pull your sword and stab the first guard, then the second guard comes at you, but the Princess takes him before he gets to you. You're quickly surrounded by guards and captured. You spend your days awaiting execution for your crimes. Finally, the day has come for you to die. You're escorted by the guards to the chopping block. You lay your head down and wonder how many people have died the way you are about to? Just before the ax comes down to sever your head from your body, you think, why did I kill all those people with so many witnesses?`,
-        death: `You die!`
+        wrongResult: `You step up to the guards as if you're going to chat with them and  quickly pull out your sword and stab the first guard, then the second guard comes at you, but the Princess takes him before he gets to you. You're quickly surrounded by guards and captured. You spend your days awaiting execution for your crimes. Finally, the day has come for you to die. You're escorted by the guards to the chopping block. You lay your head down and wonder how many people have died the way you are about to? Just before the ax comes down to sever your head from your body, you think, why did I kill those people with so many witnesses?`,
+        death: `You died`
         // img: ' ',
         // img2: ' '
     },
     part3: {
         
-        description: `“Actually sir, this is Princess Katy of Dizzylot and I am with her.  We come to claim her position as next in line for the throne” you say. “Oh well why didn’t you just say so, Princess Katy?” The gates open and you're allowed into the castle.  The castle is magnificent, the most beautiful place you've ever seen. You're in the great hall with gold and white pillars from floor to ceiling, arches resembling those of a church. At the end of the hall sits King Gary on a white marble throne. You approach cautiously.  “I have returned to reclaim my throne, Dizzy chose me as Dizzylot’s next ruler. You can leave your post as King regent.” finishes Princess Katy. “But you have been gone so long Princess, you should wait to adjust back into royal life. It looks like you have been through a lot” says King Gary. No, I don't need to adjust. I left only to bring Dizzy’s murderer to justice and now that I have completed my mission, I will take my place as rightful Queen of Dizzylot.” 
-        “Well, well, left to avenge Dizzy huh?” says Gary, “the King”.  You sense the mood of the whole room shift.  “Well I guess you shouldn't have come back yet” says Gary.  “What are you saying?” asks Katy.  With thunderous laughter, Gary says “I ordered Dizzy’s death. I am the reason your precious dog is dead.”  “WHAT,” she says enraged. Without thinking, she lunges at Gary with her dagger but misses. In a moment Gary has disabled her weapon and knocked her aside.`,
+        description: `“Actually sir, this is Princess Katy of Dizzylot and I am with her. 
+        We come to claim her position as next in line for the throne” you say.
+        “Oh well why didn’t you just say so, Princess Katy?” The gates open and you're allowed into the castle.  The castle is magnificent, the most beautiful place you've ever seen. You're in the great hall, there are gold and white pillars from floor to ceiling.  The ceiling arches resemble those of a church. At the end of the hall sits King Gary on a white marble throne. You approach cautiously.  “I have returned to reclaim my throne, Dizzy chose me as Dizzylot’s next ruler. You can leave your post as king regent.” finishes Princess Katy. “But you have been gone so long Princess, you should wait to adjust back into royal life. It looks like you have been through a lot” says King Gary. No, I don't need to adjust. I left only to bring Dizzy’s murderer to justice and now that I have completed my mission, I will take my place as rightful Queen of Dizzylot.” 
+        “Well, well, left to avenge Dizzy huh?” says Gary, “the king”.  You feel the mood of the whole room shift.
+    “Well I guess you shouldn't have come back yet” says Gary.
+    “What are you saying?” asks Katy.
+    With thunderous laughter, Gary says “I ordered Dizzy’s death. I am the reason your precious dog is dead.” 
+    “WHAT,” she says enraged. Without thinking, she lunges at King Gary with her dagger but misses. In a moment King Gary has disabled her weapon and knocked her aside.`,
         // img: $('body').css('background-image', 'url("imgs/deathscreen.
-        question: `It's your turn, you come at the Gary remembering what a different, wise King once said to you….`,
+        question: `It's your turn, you come at the King remembering what a different king once said to you….`,
         wrongChoice: 'wrong riddle',
         rightChoice: 'right riddle',
-        wrongResult: `Gary easily brushes you aside and casts a spell of death grip. You feel life being sucked out of your body. Your hands wither, your hair grays, and you die a slow and painful death.`,
-        death: `You die!`,
-        rightResult:  `Your blade strikes through the heart of Gary, the battle is over. You see the Queen as she begins to wake. She sees that you have killed evil Gary.`,
+        wrongResult: `The king easily bushes you aside and casts a spell of death grip. You feel life being sucked out of your body. Your hands wither, your hair grays, and you die a slow and painful death.`,
+        death: `You died`,
+        rightResult:  `Your blade strikes true right into the heart of the king, the battle is over. You see the Queen as she is now beginning to wake. She sees that you have killed the evil king.`,
 
-        endStory: `The coming days go by in a blur as Queen Katy makes arrangements and does her best to undo all the damage evil "King" Gary had done on the kingdom. You are knighted and appointed the chief role guard to the Queen. The Queen and her guard go down in history as one of the most powerful and fair monarchs in all the realm. From time to time you see a small white dog watching from the distance, Dizzy telling you from another world that you have done well!`
+        endStory: `The coming days go by in a blur as Queen Katy makes arrangements and does her best to undo all the damage evil King Gary had on the kingdom. You are knighted as well and appointed as the chief role guard to the queen. The Queen and her guard go down in history as one of the most powerful and fair monarchs in all the realm. From time to time you see a small white dog watching from the distance, Dizzy telling you from another world that you have done well!`
     }
 };
 
